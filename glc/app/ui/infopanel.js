ea39fe13bdd5174dbac73e251f1c7803e25fe25c
Keith Peters <kp@bit-101.com>
2015-12-07 10:24:04 -0500

    docs for parenting

--- glc/app/ui/infopanel.js
+++ glc/app/ui/infopanel.js
@@ -8,7 +8,7 @@ define(["libs/quicksettings"], function(QuickSettings) {
 		infoPanel.addInfo("keys", "Keys:<br/>P - play/pause<br/>F - choose file<br/>R - reload file<br/>G - make gif<br/>S - capture still")
 		infoPanel.addInfo("credits", "Credits:");
 		infoPanel.addInfo("creator", "Architect, coding, design, etc.: Keith Peters, kp@bit-101.com");
-		infoPanel.addInfo("testers", "Contributors: <a href='https://twitter.com/p5art'>Jerome Herr</a>, <a href='https://twitter.com/cacheflowe'>Justin Gitlin</a>, <a href='https://twitter.com/andremichelle'>Andre Michelle</a>, <a href='https://twitter.com/msurguy'>Maks Surguy</a>, <a href='https://github.com/EduardoLopes'>Eduardo Lopes</a>, <a href='https://github.com/crummy'>Malcolm Crum</a>");
+		infoPanel.addInfo("testers", "Contributors: <a href='https://twitter.com/p5art'>Jerome Herr</a>, <a href='https://twitter.com/cacheflowe'>Justin Gitlin</a>, <a href='https://twitter.com/andremichelle'>Andre Michelle</a>, <a href='https://twitter.com/msurguy'>Maks Surguy</a>, <a href='https://github.com/EduardoLopes'>Eduardo Lopes</a>, <a href='https://github.com/crummy'>Malcolm Crum</a>, <a href='https://github.com/Landerson352'>Lincoln Anderson</a>");
 		infoPanel.addInfo("encoder", "GIF Encoder: Kevin Weiner, Thibault Imbert, Kevin Kwok, Johan Nordberg");
 		infoPanel.addInfo("QS", "User interface created with <a href='https://github.com/bit101/quicksettings'>QuickSettings.js</a>.");
 		infoPanel.addButton("Close", function() {
