75c1459e40fa4d7925126373e86ff81544880469
Keith Peters <kp@bit-101.com>
2016-01-01 14:54:06 -0500

    fix docs location

--- src/build.sh
+++ src/build.sh
@@ -2,14 +2,14 @@
 r.js -o build.js
 cp glc-min.js ../app/
 
-rm -rf ../app/docs
-mkdir ../app/docs
+rm -rf ../docs
+mkdir ../docs
 
-pandoc docs/index.md -f markdown -t html5 -s -o ../app/docs/index.html -H docs/main.css
-pandoc docs/intro.md -f markdown -t html5 -s -o ../app/docs/intro.html -H docs/main.css
-pandoc docs/objects.md -f markdown -t html5 -s -o ../app/docs/objects.html -H docs/main.css
-pandoc docs/properties.md -f markdown -t html5 -s -o ../app/docs/properties.html -H docs/main.css
-pandoc docs/styles.md -f markdown -t html5 -s -o ../app/docs/styles.html -H docs/main.css
-pandoc docs/tips.md -f markdown -t html5 -s -o ../app/docs/tips.html -H docs/main.css
+pandoc docs/index.md -f markdown -t html5 -s -o ../docs/index.html -H docs/main.css
+pandoc docs/intro.md -f markdown -t html5 -s -o ../docs/intro.html -H docs/main.css
+pandoc docs/objects.md -f markdown -t html5 -s -o ../docs/objects.html -H docs/main.css
+pandoc docs/properties.md -f markdown -t html5 -s -o ../docs/properties.html -H docs/main.css
+pandoc docs/styles.md -f markdown -t html5 -s -o ../docs/styles.html -H docs/main.css
+pandoc docs/tips.md -f markdown -t html5 -s -o ../docs/tips.html -H docs/main.css
 
-cp docs/images ../app/docs/ -r
+cp docs/images ../docs/ -r
